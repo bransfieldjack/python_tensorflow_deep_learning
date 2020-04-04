66bc315d3eaa8dfa5064698053f30a4f9ffc3e2e
Jack Bransfield <jbransfield@5340L-133764-M.local>
2020-04-04 21:30:37 +1100

    Additional graph.py script and code.

--- scripts/graphs.py
+++ scripts/graphs.py
@@ -12,5 +12,40 @@ This script will construct a graph and execute it.
 """
 
 ######################################################################
-# BASIC COMPUTATIONS
+# GRAPHS
 ######################################################################
+
+n1 = tf.constant(1)
+
+n2 = tf.constant(2)
+
+n3 = n1 + n2
+
+with tf.Session() as sess:
+    result = sess.run(n3)
+    # print(result)
+
+# Tensorflow creates a default graph when its started. 
+# You can check it with print(tf.get_default_graph())
+
+g = tf.Graph()  # This adds an additional graph to tensorflow. 
+
+graph_one = tf.get_default_graph()  # The current default graph. 
+
+graph_two = tf.Graph()  # Creates a new tensorflow graph. 
+
+"""
+If I wanted to assign graph_two as the default graph withing the session:
+"""
+
+with graph_two.as_default():
+    print(graph_two is tf.get_default_graph())
+
+
+"""
+Trying to assign this outside of the session will result in the default graph being printed as true (graph_one).
+"""
+
+
+
+
