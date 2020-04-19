cd2c7e5fe0b4ee5e7691702d9adf786967f88656
Jack Bransfield <jbransfield@5340L-133764-M.local>
2020-04-20 01:27:37 +1000

    CNN basic setup script.

---
+++ keras_work/convolutional_neural_networks_basic.py
@@ -0,0 +1,25 @@
+
+
+"""
+
+https://www.youtube.com/watch?v=x_VrgWTKkiM
+
+Convolutional neural networks are useful for distinguishing between shapes contained in a single image. 
+Basic computer vision using a deep neural network is appropriate for a dataset of images such as fashion MNIST 28x28 pixels.
+This is a limitation, the images in that dataset all have their subjects centered and it was the only thing in the image. 
+
+The idea behind a CNN, is that you filter the images before training the deep neural network. 
+After filtering the images, features within them can then come to the forefront, and you can distinguish identifiable features. 
+
+A filter is simply a set of multiplyers. 
+
+"""
+
+model = tf.keras.models.Sequential([  
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu',   # Generates 64 filters, multiplying each of them acorss the image.
+                           input_shape=(28, 28, 1)),
+    tf.kerad.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),  # Dont specify an input shape, instead put a convolutional layer on top of it like above ^
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
